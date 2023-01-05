6d0137bd4d7046bd82be810e19f29ce45e07e174
Vladyslav Bondarchuk <vladyslav_bondarchuk@epam.com>
2023-01-05 12:03:29 +0200

    Test setup with Express and Typescript

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
